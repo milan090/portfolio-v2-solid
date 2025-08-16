1e85d12ff0cc5e864993e5e2bd30020ae36bc651
milan090 <milanmuhammed1@gmail.com>
2025-08-17 00:56:51 +0530

    fix build issues 2

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import glslify from 'vite-plugin-glslify';
+
 export default defineConfig({
   plugins: [solidPlugin(), glslify()],
   assetsInclude: ['**/*.woff', '**/*.woff2', '**/*.ttf'], // Add this line
+  server: { preset: "vercel" }
 });
